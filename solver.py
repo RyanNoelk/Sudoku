5a28c6714afcbafb604483b6b5ecfd68360f432d
ryann <ryann@simplyhired.com>
2016-04-03 01:19:55 -0700

    adding more comments and small code clean up

--- solver.py
+++ solver.py
@@ -17,14 +17,26 @@ class Solver:
         self.box_height = self.box_width = int(sqrt(self.height))
         self.max_value = self.height if self.height > self.width else self.width
 
+        # Predefined ranges
+        self._range_box_height = range(self.box_height)
+        self._range_box_width = range(self.box_width)
+        self._range_max_value = range(1, self.max_value + 1)
+
     def solve_puzzle(self):
+        """:return: the solved puzzle"""
         return self._solve(self.puzzle)
 
     def _solve(self, puzzle, current_y=0, current_x=0):
+        """Recursive function that solves the a sudoku puzzle using backtracking
+            :param current_x: current position of the x-axis
+            :param current_y: current position of the y-axis
+            :param puzzle: the current puzzle
+            :return: a valid puzzle or None
+        """
         y, x = self._next_empty(puzzle, current_y, current_x)
         if x is None or y is None:
             return puzzle
-        for write_val in range(1, self.max_value + 1):
+        for write_val in self._range_max_value:
             if self._validate(puzzle, y, x, write_val):
                 puzzle[y][x] = write_val
                 if self._solve(puzzle, y, x) is not None:
@@ -33,8 +45,12 @@ class Solver:
         return None
 
     def _next_empty(self, puzzle, y_start, x_start):
-        """ Returns the position of the next empty value, None if there aren't any
-            Then sets moves the pointer to the next number
+        """ Returns the position of the next empty value, none if there aren't any.
+            Then sets moves the pointer to the next number.
+            :param x_start: starting position of the x-axis
+            :param y_start: starting position of the y-axis
+            :param puzzle: the current puzzle
+            :return: the y, x coordinates of the next empty square
         """
         for y in range(y_start, self.height):
             for x in range(x_start, self.width):
@@ -45,13 +61,12 @@ class Solver:
         return None, None
 
     def _validate(self, puzzle, y, x, num):
-        """ Checks the puzzle if valid
+        """ Checks the puzzle if its valid
             :param x: x-axis location
             :param y: y-axis location
             :param num: The number to be tried
             :return: If the puzzle is valid
         """
-
         # check the row
         for val in puzzle[y]:
             if num == val:
@@ -63,8 +78,8 @@ class Solver:
         # check the box
         start_y = int(y / self.box_height) * self.box_height
         start_x = int(x / self.box_width) * self.box_width
-        for val_y in range(self.box_height):
-            for val_x in range(self.box_width):
+        for val_y in self._range_box_height:
+            for val_x in self._range_box_width:
                 if num == puzzle[val_y+start_y][val_x+start_x]:
                     return False
 
