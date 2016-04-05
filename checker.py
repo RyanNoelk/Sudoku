ed8f22379f1afc407f210c59f95ffcab0ae61369
ryann <ryann@simplyhired.com>
2016-04-05 06:36:55 -0700

    adding checker

---
+++ checker.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from math import sqrt
+
+
+class Checker:
+    """Given a user solved puzzle, this class will check if the puzzle is correctly solved.
+
+        puzzle = [
+             [9, 7, 8, 5, 6, 3, 1, 4, 2],
+             [6, 2, 3, 9, 1, 4, 5, 7, 8],
+             [5, 1, 4, 7, 8, 2, 6, 3, 9],
+             [7, 6, 2, 8, 9, 1, 4, 5, 3],
+             [1, 8, 9, 3, 4, 5, 2, 6, 7],
+             [4, 3, 5, 6, 2, 7, 8, 9, 1],
+             [3, 5, 6, 2, 7, 8, 9, 1, 4],
+             [2, 4, 7, 1, 5, 9, 3, 8, 6],
+             [8, 9, 1, 4, 3, 6, 7, 2, 5]
+         ]
+        checked = Checker(puzzle)
+        pprint(checked._compare_list())
+
+        puzzle = [
+            [2, 1, 4, 3],
+            [4, 3, 2, 1],
+            [3, 2, 1, 4],
+            [1, 4, 3, 2]
+        ]
+        checked = Checker(puzzle)
+        pprint(checked.check_puzzle())
+    """
+    def __init__(self, puzzle):
+        # The unsolved raw puzzle
+        self.puzzle = puzzle
+
+        # TODO: We are assuming that we have a square puzzle, make this work for rectangles
+        # The total box height and width
+        self.height = self.width = len(self.puzzle)
+        # The inner box height and width
+        self.box_height = self.box_width = int(sqrt(self.height))
+        # The maximum int value of the number that can be used in the puzzle
+        self.max_value = self.height if self.height > self.width else self.width
+
+        # Predefined ranges
+        self._range_box_height = range(self.box_height)
+        self._range_box_width = range(self.box_width)
+        self._range_max_value = range(1, self.max_value + 1)
+
+    def _is_complete(self):
+        """:return: False if there is still an empty space left in the puzzle, True otherwise."""
+        for y in range(self.height):
+            for x in range(self.width):
+                if 0 == self.puzzle[y][x]:
+                    return False
+        return True
+
+    def _compare_list(self, my_list):
+        """Compare a list to self._range_max_value to see if they have the same contains
+            :return: True if they are the same, False otherwise."""
+        if my_list == self._range_max_value:
+            pass
+
+        return True
+
+    def validate(self):
+        """:return: True if the puzzle is solve correctly, False otherwise."""
+
+        # check if the puzzle is complete
+        if not self._is_complete():
+            return False
+
+        # check the row
+        for my_list in self.puzzle:
+            if self._compare_list(my_list) is False:
+                return False
+
+        # check the column
+        # TODO: need to invert then compare a list to see if they contain the same numbers.
+        for my_list in self.puzzle:
+            if self._compare_list(my_list) is False:
+                return False
+
+        # build the box
+        for box_num in self._range_max_value:
+            box_list = []
+
+            y_offset = 0
+            if box_num == 4 or box_num == 5 or box_num == 6:
+                y_offset = 3
+            if box_num == 7 or box_num == 8 or box_num == 9:
+                y_offset = 6
+
+            x_offset = 0
+            if box_num == 2 or box_num == 5 or box_num == 8:
+                x_offset = 3
+            if box_num == 3 or box_num == 6 or box_num == 9:
+                x_offset = 6
+
+            for y in self._range_box_height:
+                for x in self._range_box_width:
+                    box_list.append(self.puzzle[y + y_offset][x + x_offset])
+            # check the box
+            print box_list
+            if self._compare_list(box_list) is False:
+                return False
+
+        return True
--- runner.py
+++ runner.py
@@ -4,6 +4,8 @@
 
 from generator import Generator
 from solver import Solver
+from checker import Checker
+
 from pprint import pprint
 
 # Generate a puzzle and solve it
@@ -17,7 +19,7 @@ raw_puzzle = [
     [0, 2, 0, 0],
     [0, 0, 3, 0]
 ]
-pprint(Solver(raw_puzzle).solve_puzzle())
+#pprint(Solver(raw_puzzle).solve_puzzle())
 
 raw_puzzle = [
     [0, 7, 8, 5, 6, 0, 1, 0, 0],
@@ -30,4 +32,25 @@ raw_puzzle = [
     [0, 4, 7, 0, 0, 0, 3, 8, 0],
     [0, 0, 1, 0, 3, 6, 7, 2, 0]
 ]
-pprint(Solver(raw_puzzle).solve_puzzle())
+#pprint(Solver(raw_puzzle).solve_puzzle())
+
+
+# Example puzzles for checking the puzzle
+puzzle = [
+    [9, 7, 8, 5, 6, 3, 1, 4, 2],
+    [6, 2, 3, 9, 1, 4, 5, 7, 8],
+    [5, 1, 4, 7, 8, 2, 6, 3, 9],
+    [7, 6, 2, 8, 9, 1, 4, 5, 3],
+    [1, 8, 9, 3, 4, 5, 2, 6, 7],
+    [4, 3, 5, 6, 2, 7, 8, 9, 1],
+    [3, 5, 6, 2, 7, 8, 9, 1, 4],
+    [2, 4, 7, 1, 5, 9, 3, 8, 6],
+    [8, 9, 1, 4, 3, 6, 7, 2, 5]
+]
+pprint (puzzle)
+print
+print
+print
+
+checked = Checker(puzzle)
+pprint(checked.validate())
