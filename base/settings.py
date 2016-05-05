eb2381c67c3ea977fb7ac5f5fee3202662e30cd2
RyanNoelk <ryannoelk@gmail.com>
2016-05-04 22:00:22 -0700

    adding some base stuff

--- base/settings.py
+++ base/settings.py
@@ -32,7 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'play'
+    'base',
+    'play',
 ]
 
 MIDDLEWARE_CLASSES = [
