894b1eb30bb9543620f49474ed4f5950d496e89e
ryann <ryann@simplyhired.com>
2016-05-22 23:15:01 -0700

    fixing header urls and indents

--- about/urls.py
+++ about/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^contact/$', views.AboutView.as_view(), name='contact'),
-    url(r'^$', views.ContactView.as_view(), name='about'),
+    url(r'^contact/$', views.ContactView.as_view(), name='contact'),
+    url(r'^$', views.AboutView.as_view(), name='about'),
 ]
